bbe661fa1b8cfd6428b1cf8a9f9ac73f1683dadd
Kosta Korenkov <kosta@korenkov.net>
2020-06-01 20:30:54 +0300

    tests: fix

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('connect button is available', () => {
+test('connect button is available', (done) => {
   const { getByText } = render(<App />);
-  expect(getByText(/Connect/)).toBeInTheDocument();
-  expect(
-    getByText(/Please connect your wallet first/),
-  ).toBeInTheDocument();
+  setImmediate(() => {
+    expect(getByText(/Connect/)).toBeInTheDocument();
+    expect(
+      getByText(/Please connect your wallet first/),
+    ).toBeInTheDocument();
+    done();
+  });
 });
